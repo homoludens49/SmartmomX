20d07a0057789fdb9dea2b6c5a2b194ccdd03119
Pjotrs Sakov <sakov.p@gmail.com>
2019-11-02 11:48:16 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Smartmom.Startup))]
+namespace Smartmom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
