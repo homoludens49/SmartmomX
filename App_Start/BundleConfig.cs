70358f4ea168c321883b71d0d7a4b550decbe292
Pjotrs Sakov <sakov.p@gmail.com>
2019-11-02 12:59:20 +0200

    tst

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Smartmom
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap_darkly.css",
                       "~/Content/site.css"));
         }
     }
---
+++ Controllers/ItemController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Smartmom.Models;
+
+namespace Smartmom.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Random()
+        {
+            var item = new Item() { Name = "Alilo" };
+           
+          //  return View(item);
+            return HttpNotFound();
+        }
+    }
+}
